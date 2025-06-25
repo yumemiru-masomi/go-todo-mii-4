270244275f9aff914a306735e10e056deeb95b93
yumemiru-masomi <yumemi.saito@blueish.co.jp>
2025-06-25 23:11:30 +0900

    feat: signout機能を追加

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -2,5 +2,5 @@
 import { redirect } from "next/navigation";
 
 export default function Page() {
-  redirect("/home");
+  redirect("auth/signin");
 }
--- next/src/components/Header.tsx
+++ next/src/components/Header.tsx
@@ -4,6 +4,7 @@
 import { useRouter } from "next/navigation";
 import { format, addDays, parseISO } from "date-fns";
 import { ja } from "date-fns/locale";
+import { Bars3Icon } from "@heroicons/react/24/solid";
 
 type Props = {
   date: string; // "2025-04-17"
@@ -26,35 +27,46 @@ export default function TodoHeader({ date }: Props) {
   };
 
   return (
-    <div className="flex items-center justify-between py-10">
-      {/* 今日ボタン */}
-      <button
-        onClick={goToday}
-        className="px-3 py-1 rounded-full border border-gray-300 text-sm hover:bg-gray-100 transition"
-      >
-        今日
-      </button>
-      {/* 中央：日付 */}
-      <div className="text-center text-lg font-semibold tracking-wide">
-        {format(currentDate, "yyyy年 M月 d日 (E)", { locale: ja })}
-      </div>
+    <>
+      <div className="flex items-center justify-between py-10">
+        {/* ハンバーガーアイコン */}
+        <button
+          onClick={() => router.push("/auth/signout")}
+          aria-label="メニューを開く"
+          className="p-2"
+        >
+          <Bars3Icon className="w-6 h-6 text-white" />
+        </button>
 
-      <div className="flex items-center">
-        {/* 左ボタン */}
+        {/* 今日ボタン */}
         <button
-          onClick={() => goToDate(-1)}
-          className="text-2xl font-bold px-2 hover:text-blue-500 transition"
+          onClick={goToday}
+          className="px-3 py-1 rounded-full border border-gray-300 text-sm hover:bg-gray-100 transition"
         >
-          ←
+          今日
         </button>
-        {/* 右ボタン */}
-        <button
-          onClick={() => goToDate(1)}
-          className="text-2xl font-bold px-2 hover:text-blue-500 transition"
-        >
-          →
-        </button>
+        {/* 中央：日付 */}
+        <div className="text-center text-lg font-semibold tracking-wide">
+          {format(currentDate, "yyyy年 M月 d日 (E)", { locale: ja })}
+        </div>
+
+        <div className="flex items-center">
+          {/* 左ボタン */}
+          <button
+            onClick={() => goToDate(-1)}
+            className="text-2xl font-bold px-2 hover:text-blue-500 transition"
+          >
+            ←
+          </button>
+          {/* 右ボタン */}
+          <button
+            onClick={() => goToDate(1)}
+            className="text-2xl font-bold px-2 hover:text-blue-500 transition"
+          >
+            →
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
